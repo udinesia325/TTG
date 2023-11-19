3d23a616c5d134b90a729098ffd2f485403f246c
udinesia325 <udinesia325@gmail.com>
2023-11-19 20:54:35 +0700

    [API CRUD] rest api done

--- API CRUD/server/app/Http/Controllers/TodoController.php
+++ API CRUD/server/app/Http/Controllers/TodoController.php
@@ -2,9 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreTodoRequest;
+use App\Http\Resources\TodoResource;
+use App\Models\Todo;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class TodoController extends Controller
 {
     //
+    function index():JsonResponse {
+        $todos = Todo::paginate(10);
+        return response()->json([
+            "todo" => new TodoResource($todos)
+        ]);
+    }
+
+    function show(Todo $todo):JsonResponse {
+        return response()->json($todo);
+    }
+
+    function create(StoreTodoRequest $request):JsonResponse {
+        $todo = new Todo();
+        $todo->title = $request->input('title');
+        $todo->description = $request->input('description');
+
+        $todo->save();
+        
+        return response()->json($todo);
+    }
+    
+    function update(Request $request,Todo $todo):JsonResponse {
+        $todo->title = $request->input('title');
+        $todo->description = $request->input('description');
+        $todo->save();
+        return response()->json($todo);
+    }
+    
+    function delete(Todo $todo):JsonResponse {
+        $todo->delete();
+        return response()->json($todo);
+    }
 }
--- API CRUD/server/routes/api.php
+++ API CRUD/server/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TodoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/GetAll',[TodoController::class,'index']);
+Route::post('/Create',[TodoController::class,'create']);
+
+function missing(){
+
+    return fn() => response()->json([
+        "message" => "Not Found"
+    ]);
+}
+Route::get('/GetOne/{todo}',[TodoController::class,'show'])->missing(missing());
+Route::patch('/Update/{todo}',[TodoController::class,'update'])->missing(missing());
+Route::delete('/Delete/{todo}',[TodoController::class,'delete'])->missing(missing());
